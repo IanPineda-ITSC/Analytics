b2b32a7a7403e43c60850bad0b93a6cc765cb513
Ian Pineda <ian.bravo@itsmartconnected360.com>
2023-09-15 10:47:22 -0600

    Nuevo calculo de semanales

--- DP/inactivos/semanal.sql
+++ DP/inactivos/semanal.sql
@@ -1,37 +1,39 @@
 WITH
 
-SEMANAS AS (
+RANGO_DE_FECHAS AS (
     SELECT
+        ANIO_ALSEA,
         MES_ALSEA,
         SEM_ALSEA,
-        ANIO_ALSEA,
-        max(to_date(FECHA)) AS DOMINGO,
-        DOMINGO - 180 AS FECHA_INICIO_ACTIVOS,
-        DOMINGO - 365 AS FECHA_FIN_PERDIDOS
+        max(to_date(FECHA)) AS FECHA_FIN,
+        FECHA_FIN - 180 AS FECHA_INICIO_ACTIVOS,
+        FECHA_FIN - 365 AS FECHA_INICIO_INACTIVOS
     FROM
         WOW_REWARDS.WORK_SPACE_WOW_REWARDS.DS_DIM_TIME
     WHERE
     (
+        ANIO_ALSEA = 2022
+    )
+    OR
+    (
         ANIO_ALSEA = 2023
         AND
-        SEM_ALSEA = 36
+        SEM_ALSEA <= 36
     )
-    -- OR
-    -- (
-    --     ANIO_ALSEA = 2022
-    --     AND
-    --     SEM_ALSEA >= 32
-    -- )
     GROUP BY
-        SEM_ALSEA,
+        ANIO_ALSEA,
         MES_ALSEA,
-        ANIO_ALSEA
+        SEM_ALSEA
 ),
 
 TRANSACCIONES_OLO AS (
     SELECT DISTINCT
         lower(MXP.EMAIL) AS EMAIL,
-        to_date(OLO.ORDER_DATE) AS FECHA
+        ORDER_DATE AS FECHA,
+        TO_CHAR(ORDER_DATE,'YYYY-MM-DD') || '-' || LOCATION_CODE || '-' || OLO.ORDER_NUMBER AS ORDER_ID,
+        OLO.ORDERFINALPRICE / 1.16 AS ORDER_AMOUNT,
+        PHONENUMBER AS PHONE,
+        FIRSTNAME
     FROM
        "SEGMENT_EVENTS"."DOMINOS_OLO"."MXPOWERSALESLOG" MXP
     INNER JOIN 
@@ -45,62 +47,87 @@ TRANSACCIONES_OLO AS (
     AND 
         OLO.LOCATION_CODE NOT IN ('13001' , '13006', '13021', '11000')
     AND 
-        UPPER(OLO.SOURCE_CODE) IN ('ANDROID' , 'DESKTOP', 'IOS', 'MOBILE', 'WEB', 'ANDROID2', 'DESKTOP2', 'IOSAPP', 'MOBILE2', 'WHATSAPP')
+        UPPER(OLO.SOURCE_CODE) IN (
+            'ANDROID', 
+            'DESKTOP', 
+            'IOS', 
+            'MOBILE', 
+            'WEB', 
+            'ANDROID2', 
+            'DESKTOP2', 
+            'IOSAPP', 
+            'MOBILE2', 
+            'WHATSAPP'
+        )
 ),
 
 TRANSACCIONES_CLOUD AS (
     SELECT DISTINCT 
         lower(A.EMAIL) AS EMAIL,
-        to_date(SUBSTRING(A.STOREORDERID,1,10)) AS FECHA
+        to_date(SUBSTRING(A.STOREORDERID,1,10)) AS FECHA,
+        split_part(A.STOREORDERID, '#', 1) || '-' || A.STOREID || '-' || split_part(A.STOREORDERID, '#', 2) AS ORDER_ID,
+        PAYMENTSAMOUNT / 1.16 AS ORDER_AMOUNT,
+        PHONE,
+        FIRSTNAME
     FROM 
         "SEGMENT_EVENTS"."DOMINOS_GOLO"."VENTA_CLOUD" A
     WHERE 
         A.STOREID NOT LIKE '9%'
     AND 
-        A.SOURCEORGANIZATIONURI IN ('order.dominos.com','resp-order.dominos.com','iphone.dominos.mx','android.dominos.mx') 
-    AND 
-        A.SOURCEORGANIZATIONURI IS NOT NULL
+        A.SOURCEORGANIZATIONURI IN (
+            'order.dominos.com', 
+            'resp-order.dominos.com', 
+            'iphone.dominos.mx', 
+            'android.dominos.mx'
+        )
 ),
 
-TRANSACCIONES_TOTAL AS (
-    SELECT * FROM TRANSACCIONES_CLOUD
-    UNION ALL
+TRANSACCIONES_OLO_Y_CLOUD AS (
     SELECT * FROM TRANSACCIONES_OLO
+    UNION ALL
+    SELECT * FROM TRANSACCIONES_CLOUD
 ),
 
-INACTIVOS AS (
+SEGMENTACION_BASE_INACTIVOS AS (
     SELECT
-        EMAIL,
+        ANIO_ALSEA,
         MES_ALSEA,
         SEM_ALSEA,
-        ANIO_ALSEA
+        EMAIL,
+        'INACTIVOS' AS SEGMENTO
     FROM
-        TRANSACCIONES_TOTAL
-    INNER JOIN
-        SEMANAS
+        TRANSACCIONES_OLO_Y_CLOUD
+    JOIN
+        RANGO_DE_FECHAS
     ON
-        FECHA <= DOMINGO
+        FECHA < FECHA_FIN
     GROUP BY
-        EMAIL,
+        ANIO_ALSEA,
         MES_ALSEA,
         SEM_ALSEA,
-        ANIO_ALSEA
+        EMAIL,
+        FECHA_INICIO_ACTIVOS,
+        FECHA_INICIO_INACTIVOS
     HAVING
-        avg(CASE WHEN FECHA_INICIO_ACTIVOS >= FECHA AND FECHA > FECHA_FIN_PERDIDOS  THEN 1 ELSE 0 END) = 1
+        max(FECHA) < FECHA_INICIO_ACTIVOS
+    AND
+        FECHA_INICIO_INACTIVOS <= max(FECHA)
 )
 
 SELECT
     ANIO_ALSEA,
     MES_ALSEA,
     SEM_ALSEA,
+    SEGMENTO,
+    count(DISTINCT EMAIL),
+    count(DISTINCT lower(EMAIL)),
     count(*)
 FROM
-    INACTIVOS
+    SEGMENTACION_BASE_INACTIVOS
 GROUP BY
-    MES_ALSEA,
     ANIO_ALSEA,
-    SEM_ALSEA
-ORDER BY
-    ANIO_ALSEA DESC,
-    MES_ALSEA DESC,
-    SEM_ALSEA DESC
+    MES_ALSEA,
+    SEM_ALSEA,
+    SEGMENTO
+;
+--  902,435
